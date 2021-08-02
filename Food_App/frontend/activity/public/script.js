3440eefb857df7cbe6df4f508d9b749d42555561
sachinsharma0237 <sachinsharma0237@gmail.com>
2021-08-03 00:18:55 +0530

    responsiv web

--- Food_App/frontend/activity/public/script.js
+++ Food_App/frontend/activity/public/script.js
@@ -4,8 +4,20 @@ let idx = 0;
 let word = names[idx];
 let text = "";
 let isDeleting = false;
+let showcase = document.querySelector(".showcase");
+let navlinks = document.querySelector(".navlinks")
+
+
 window.addEventListener('load', ()=>{
     typeWords();
+    window.addEventListener('scroll', ()=>{
+        let {bottom} = showcase.getBoundingClientRect();
+        if( bottom < 0 ){
+            navlinks.classList.add(".fixed");
+        }else{
+            navlinks.classList.remove(".fixed");
+        }
+    })
 })
 
 function typeWords(){
